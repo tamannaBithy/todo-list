8a58e628cc7c0584d7d7d04cd63fe8621a055204
Tamanna14-cpu <bimurto.bithy@gmail.com>
2021-12-27 12:58:20 +0600

    delete button is working

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,12 +1,31 @@
 import React from 'react';
 
 const TodoList = ({ todos, setTodos }) => {
+
+    const handleDelete = ({ id }) => {
+        setTodos(todos.filter(todo => todo.id !== id))
+    }
+
+
     return (
         <div>
             {
                 todos.map(todo => (
                     <li key={todo.id}>
                         <input type="text" value={todo.title} onChange={e => e.preventDefault()} />
+
+                        <div>
+                            <button>
+                                <i class="fas fa-check-circle"></i>
+                            </button>
+                            <button>
+                                <i class="fas fa-edit"></i>
+                            </button>
+                            <button onClick={() => handleDelete(todo)}>
+                                <i class="far fa-trash-alt"></i>
+                            </button>
+                        </div>
+
                     </li>
                 ))
             }
